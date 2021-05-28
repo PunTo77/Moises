adf100ac17dd74894786d10efd1657d97120cbaa
sergio <cf20sergio.prieto@iesjoandaustria.org>
2021-05-28 17:35:26 +0200

    3 push REAL

--- Moises/src/moises/Client.java
+++ Moises/src/moises/Client.java
@@ -44,7 +44,7 @@ public class Client {
                 getNom() +
                 " (" + getNif() + ")\n";
         for (Lloguer lloguer: lloguers) {
-        	 double quantitat = Lloguer.quantitatPerLloguer(lloguer)
+        	 double quantitat = Lloguer.quantitatPerLloguer(lloguer);
         	/*double quantitat = 0;
             switch (lloguer.getVehicle().getCategoria()) {
                 case Vehicle.BASIC:
@@ -86,27 +86,5 @@ public class Client {
         resultat += "Import a pagar: " + total + "€\n" +
                 "Punts guanyats: " + bonificacions + "\n";
         return resultat;
-    }
-    
-    /*public double quantitatPerLloguer(Lloguer lloguer) {
-    	double quantitat = 0;
-        switch (lloguer.getVehicle().getCategoria()) {
-            case Vehicle.BASIC:
-                quantitat += 3;
-                if (lloguer.getDies() > 3) {
-                    quantitat += (lloguer.getDies() - 3) * 1.5;
-                }
-                break;
-            case Vehicle.GENERAL:
-                quantitat += 4;
-                if (lloguer.getDies() > 2) {
-                    quantitat += (lloguer.getDies() - 2) * 2.5;
-                }
-                break;
-            case Vehicle.LUXE:
-                quantitat += lloguer.getDies() * 6;
-                break;
-        }*/
-		return quantitat;
     }
 }
