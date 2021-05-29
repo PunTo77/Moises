919ee144001e6dea04b6d63690e660c2edbab15c
sergio <cf20sergio.prieto@iesjoandaustria.org>
2021-05-29 23:16:37 +0200

    5push

--- Moises/src/moises/Lloguer.java
+++ Moises/src/moises/Lloguer.java
@@ -1,43 +1,26 @@
 package moises;
 
-import javax.swing.*;
 import java.util.Date;
- 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+  
 public class Lloguer {
-    private Date data;
-    private int dies;
-    private Vehicle vehicle;
-    public Lloguer(Date date, int i, Vehicle vehicleBasic) {
-        this.data = date;
-        this.dies = i;
-        this.vehicle = vehicleBasic;
-    }
- 
-    public Date getData() {
-        return data;
-    }
- 
-    public void setDate(Date date) {
-        this.data = date;
-    }
- 
-    public int getDies() {
-        return dies;
-    }
- 
-    public void setDies(int dies) {
-        this.dies = dies;
-    }
- 
-    public Vehicle getVehicle() {
-        return vehicle;
-    }
- 
-    public void setVehicle(Vehicle vehicle) {
-        this.vehicle = vehicle;
-    }
+	private Date data;
+	private int dies;
+	private Vehicle vehicle;
+	private double quantitat;
+	
+	public Lloguer(Date data, int dies,  Vehicle vehicle) {
+		super();
+		this.data = data;
+		this.dies = dies;
+		this.vehicle = vehicle;
+	}
+	public Date getData() { return data; }
+	public int getDies() { return dies;	}
+	public Vehicle getVehicle() { return vehicle; }
     
-    public double quantitat() {
+	public double quantitat() {
     	double quantitat = 0;
         switch (this.getVehicle().getCategoria()) {
             case Vehicle.BASIC:
@@ -45,7 +28,6 @@ public class Lloguer {
                 if (this.getDies() > 3) {
                     quantitat += (this.getDies() - 3) * 1.5;
                 }
-                break;
             case Vehicle.GENERAL:
                 quantitat += 4;
                 if (this.getDies() > 2) {
@@ -56,7 +38,15 @@ public class Lloguer {
                 quantitat += this.getDies() * 6;
                 break;
         }
-        return quantitat; 
-        
-    	}
+		return quantitat;
+    }
+    public int bonificacions() {
+	    int bonificacions = 0;
+	    bonificacions++;
+    	if (this.getVehicle().getCategoria() == Vehicle.LUXE &&
+	            this.getDies()>1 ) {
+	        bonificacions++;
+	    }
+    	return bonificacions;
+    }
 }
--- Moises/src/moises/Main.java
+++ Moises/src/moises/Main.java
@@ -3,29 +3,37 @@ package moises;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
- 
-public class Main {
-    public static void main(String[] args) throws ParseException {
-        // demostració de construcció d'un vehicle de categoria BASIC
-        Vehicle vehicleBasic1 = new Vehicle("Tata", "Vista", Vehicle.BASIC);
-        Vehicle vehicleBasic2 = new Vehicle("Toto", "Visto", Vehicle.BASIC);
-        Vehicle vehicleBasic3 = new Vehicle("Tete", "Viste", Vehicle.BASIC);
- 
-        // demostració de construccuó d'un lloguer amb una data
-        SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
-        Date date = dateFormat.parse("2/8/2013");
-        Lloguer lloguerBasic1 = new Lloguer(date, 2, vehicleBasic1);
-        Lloguer lloguerBasic2 = new Lloguer(date, 2, vehicleBasic2);
-        Lloguer lloguerBasic3 = new Lloguer(date, 2, vehicleBasic3);
- 
-        Client cliente = new Client("123456R", "Bob en manetes", "666777888");
-        cliente.afegeix(lloguerBasic1);
-        cliente.afegeix(lloguerBasic2);
-        cliente.afegeix(lloguerBasic3);
-        cliente.informe();
-    }
-}
+  
+public class Main { 
 
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyy");
+		
+		Vehicle vehicle1 = new Vehicle("Tata", "Vista", Vehicle.BASIC);
+		Date date = dateFormat.parse("2/8/2013");
+		Lloguer lloguer1 = new Lloguer(date, 2, vehicle1);
+		
+		Vehicle vehicle2 = new Vehicle("Mazda", "v3", Vehicle.GENERAL);
+		date = dateFormat.parse("12/8/2006");
+		Lloguer lloguer2 = new Lloguer(date, 5, vehicle2);
+		
+		Vehicle vehicle3 = new Vehicle("Audi", "Q7", Vehicle.LUXE);
+		date = dateFormat.parse("2/10/2020");
+		Lloguer lloguer3 = new Lloguer(date, 7, vehicle3);
+		
+		Client c = new Client("23232323Y", "Leo Messi", "565656565");
+		c.afegeix(lloguer1);
+		c.afegeix(lloguer2);;
+		c.afegeix(lloguer3);
+		System.out.print(GestorLloguersLite(c));
+		
+	}
+	public static String GestorLloguersLite(Client c){
+        return c.informe();
+    }
+
+}
 
 
 
